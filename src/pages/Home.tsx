5597ac53a29990bafffc0b3045a3255a5d3244d6
Ubinquitous <ubinquitous1@gmail.com>
2022-10-06 19:49:28 +0900

    ADD : Header 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Header } from 'src/allFiles';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
