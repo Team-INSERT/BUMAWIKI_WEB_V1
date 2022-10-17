85949a6bb6df9bb88e36fb51ad2689b3f43c7719
Ubinquitous <ubinquitous1@gmail.com>
2022-10-17 21:52:33 +0900

    UPDATE : 화살표 함수표현식으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './allFiles';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
