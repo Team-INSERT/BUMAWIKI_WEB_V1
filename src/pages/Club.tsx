ceba9c5eb49c74623468f6d7d01d72cad5acd0ef
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:29:01 +0900

    ADD : Accident InitialCommit

---
+++ src/pages/Club.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'src/allFiles';
+
+const Club = () => {
+    return (
+        <div>
+            <Header />
+            전공동아리목록
+        </div>
+    );
+};
+
+export default Club;
