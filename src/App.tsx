83f20dd376c2e0b33e95633deb3d61579ac79244
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:31:00 +0900

    ADD : Router 추가, BrowserRouter로 import명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './allFiles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Home,
+  Student,
+  Teacher,
+  Accident,
+  Club,
+  StudentInfo,
+  TeacherInfo,
+  AccidentInfo,
+  ClubInfo
+} from './allFiles';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path={'/'} element={<Home />} />
+        <Route path={'/student'} element={<Student />} />
+        <Route path={'/teacher'} element={<Teacher />} />
+        <Route path={'/accident'} element={<Accident />} />
+        <Route path={'/club'} element={<Club />} />
+        <Route path={'/student/:name'} element={<StudentInfo />} />
+        <Route path={'/teacher'} element={<TeacherInfo />} />
+        <Route path={'/accident'} element={<AccidentInfo />} />
+        <Route path={'/club'} element={<ClubInfo />} />
       </Routes>
     </Router>
   );
