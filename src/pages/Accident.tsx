b1e67ecdd51b9561d8d369286ca879c6691ede49
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:28:50 +0900

    ADD : Accident InitialCommit

---
+++ src/pages/Accident.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'src/allFiles';
+
+const Accident = () => {
+    return (
+        <div>
+            <Header />
+            사건사고목록
+        </div>
+    );
+};
+
+export default Accident;
