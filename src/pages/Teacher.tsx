a4fbebe50393839b6d93f96e9ab9add5de003552
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:29:34 +0900

    ADD : Teacher InitialCommit

---
+++ src/pages/Teacher.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'src/allFiles';
+
+const Teacher = () => {
+    return (
+        <div>
+            <Header />
+            선생님 목록
+        </div>
+    );
+};
+
+export default Teacher;
