8f1f91a49760020531dc3c69cd46e4931bc84fc9
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:30:42 +0900

    ADD : import 파일 추가

--- src/allFiles.ts
+++ src/allFiles.ts
@@ -1,7 +1,23 @@
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Student from "./pages/Student";
+import Accident from "./pages/Accident";
+import Club from "./pages/Club";
+import Teacher from "./pages/Teacher";
+import StudentInfo from "./pages/Details/StudentInfo";
+import AccidentInfo from "./pages/Details/AccidentInfo";
+import ClubInfo from "./pages/Details/ClubInfo";
+import TeacherInfo from "./pages/Details/TeacherInfo";
 
 export {
     Home,
     Header,
+    Student,
+    Accident,
+    Club,
+    Teacher,
+    StudentInfo,
+    AccidentInfo,
+    ClubInfo,
+    TeacherInfo
 };
