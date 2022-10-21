832d008833d02fa3b1554c0ecc27cc5db202f95a
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 21:31:13 +0900

    ADD : Footer import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { Header } from 'src/allFiles';
+import { Header, Footer } from 'src/allFiles';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     );
 };
