38cfdaa4e45fa4a12fc2fa7a7e5462ff67c51cbb
Ubinquitous <ubinquitous1@gmail.com>
2022-10-21 20:29:11 +0900

    ADD : Accident InitialCommit

---
+++ src/pages/Student.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'src/allFiles';
+
+const Student = () => {
+    return (
+        <div>
+            <Header />
+            학생 목록
+        </div>
+    );
+};
+
+export default Student;
