533681ab7a3d1e44ca0a090fd63e2ba73a70d85a
Ubinquitous <ubinquitous1@gmail.com>
2022-10-22 12:52:47 +0900

    ADD : 백보드 스타일링

---
+++ src/style/Board.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Board = styled.div`
+	width: 72vw;
+	height:1000px;
+	background-color: #fff;
+	border-left: 2px solid #ccc;
+	border-right: 2px solid #ccc;
+	margin: 0 1.6vw 0 6vw;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+
+export default Board;
