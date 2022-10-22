d6c8f3069620637a91f6e8ef3a8efae8b589d8ac
Ubinquitous <ubinquitous1@gmail.com>
2022-10-22 23:17:47 +0900

    UPDATE : 요소 10개로 확대

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -4,7 +4,7 @@ import '../style/Aside.scss';
 const Aside = () => {
     return (
         <div className='aside-wrap'>
-            {/* 글자가 넘어갈 경우 슬라이싱 필요, map 사용 필요 (요소개수 : 8개) */}
+            {/* 글자가 넘어갈 경우 슬라이싱 필요, map 사용 필요 (요소개수 : 10개) */}
             <div className='aside-title-box'>
                 <span>인기 문서</span>
             </div>
@@ -21,16 +21,22 @@ const Aside = () => {
                 <span>친구없는 화법을 쓰시네 (사건사고)</span>
             </div>
             <div className='aside-doc-box'>
-                <span>박우빈</span>
+                <span>박우빈 (학생)</span>
             </div>
             <div className='aside-doc-box'>
-                <span>AK-47</span>
+                <span>AK-47 (학생)</span>
             </div>
             <div className='aside-doc-box'>
-                <span>T자형 인재</span>
+                <span>T자형 인재 (동아리)</span>
             </div>
             <div className='aside-doc-box'>
                 <span>xxx (선생님)</span>
+            </div>
+            <div className='aside-doc-box'>
+                <span>히히 기숙사 못들어와 (사건사고)</span>
+            </div>
+            <div className='aside-doc-box' style={{ borderBottom: 'none' }}>
+                <span>저 피자만 가지고 가려고... (사건사고)</span>
             </div>
         </div>
     );
