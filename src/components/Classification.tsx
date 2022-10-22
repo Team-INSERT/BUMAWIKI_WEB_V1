fcc02203b8b91de02a53b2c0eb2f7792c383e096
Ubinquitous <ubinquitous1@gmail.com>
2022-10-22 12:51:45 +0900

    ADD : 분류 컴포넌트 추가

---
+++ src/components/Classification.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react';
+import '../style/Classification.scss';
+
+const Classification = ({ children }: PropsWithChildren) => {
+    return (
+        <div className='classif-wrap'>
+            <span className='classif'>분류 :&nbsp;</span>
+            <span className='classif-case'>{children}</span>
+        </div>
+    );
+};
+
+export default Classification;
