bca0bbdd6b51db2d3ab3f13c5ef5fed17a2f06ff
Ubinquitous <ubinquitous1@gmail.com>
2022-10-22 11:57:28 +0900

    TEST : 컴포넌트 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { Header, Footer } from 'src/allFiles';
+import { Header, Footer, Aside } from 'src/allFiles';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             <Header />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 };
