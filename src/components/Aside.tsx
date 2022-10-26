11b8ec5f90530079a78ef7b6b772c2c1cca17aca
Ubinquitous <ubinquitous1@gmail.com>
2022-10-26 22:00:54 +0900

    UPADTE : 목록 span 태그에서 Link 태그로 변경

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/components-style/Aside.scss';
 
 const Aside = () => {
@@ -9,34 +10,34 @@ const Aside = () => {
                 <span>인기 문서</span>
             </div>
             <div className='aside-doc-box'>
-                <span>권세원 (학생)</span>
+                <Link to={''} className='list'>권세원 (학생)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>중간고사 당일 짜장면 탈옥 (사건사고)</span>
+                <Link to={''} className='list'>중간고사 당일 짜장면 탈옥 (사건사고)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>프론트에게 서버 클론 시전 (사건사고)</span>
+                <Link to={''} className='list'>프론트에게 서버 클론 시전 (사건사고)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>친구없는 화법을 쓰시네 (사건사고)</span>
+                <Link to={''} className='list'>친구없는 화법을 쓰시네 (사건사고)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>박우빈 (학생)</span>
+                <Link to={''} className='list'>박우빈 (학생)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>AK-47 (학생)</span>
+                <Link to={''} className='list'>AK-47 (학생)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>T자형 인재 (동아리)</span>
+                <Link to={''} className='list'>T자형 인재 (동아리)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>xxx (선생님)</span>
+                <Link to={''} className='list'>xxx (선생님)</Link>
             </div>
             <div className='aside-doc-box'>
-                <span>히히 기숙사 못들어와 (사건사고)</span>
+                <Link to={''} className='list'>히히 기숙사 못들어와 (사건사고)</Link>
             </div>
             <div className='aside-doc-box' style={{ borderBottom: 'none' }}>
-                <span>저 피자만 가지고 가려고... (사건사고)</span>
+                <Link to={''} className='list'>저 피자만 가지고 가려고... (사건사고)</Link>
             </div>
         </div>
     );
