c9d2fc8b33287ca5c2ee171433c7600372e961a2
Ubinquitous <ubinquitous1@gmail.com>
2022-10-26 22:07:07 +0900

    ADD : ClubInfo 기본 설정

--- src/pages/Details/ClubInfo.tsx
+++ src/pages/Details/ClubInfo.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+    Header,
+    Board,
+    SubFooter,
+    Aside,
+    Footer
+} from 'src/allFiles';
+import '../../style/pages-style/details-style/ClubInfo.scss';
 
 const ClubInfo = () => {
     return (
         <div>
-
+            <Header />
+            {/* <Board>
+                <SubFooter />
+            </Board>
+            <Aside />
+            <Footer /> */}
         </div>
     );
 };
