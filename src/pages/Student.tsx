fbae05e7478c78068ee697c390b6a884ee6c92d1
Ubinquitous <ubinquitous1@gmail.com>
2022-10-26 22:01:40 +0900

    ADD : student 기본설정

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Header } from 'src/allFiles';
+import {
+    Header,
+    Board,
+    SubFooter,
+    Aside,
+    Footer
+} from 'src/allFiles';
 
 const Student = () => {
     return (
-        <div>
+        <div className='student-wrap'>
             <Header />
-            학생 목록
+            {/* <Board>
+                <SubFooter />
+            </Board>
+            <Aside />
+            <Footer /> */}
         </div>
     );
 };
