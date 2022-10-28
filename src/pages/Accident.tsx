7296729d96fbc90408fbb7eb7c84fa4c343e7e35
5ewon06 <gwonsewon03@gmail.com>
2022-10-28 20:24:15 +0900

    DELETE : 학생, 선생님 분류 삭제

--- src/pages/Accident.tsx
+++ src/pages/Accident.tsx
@@ -25,30 +25,20 @@ const Accident = () => {
                     <div className="line" />
                     {/* 사건/사고 목록 */}
                     <div className='summary-wrap'>
-                        <AccodianMenu name={'학생'}>
+                        <AccodianMenu name={'사건/사고'}>
                             <p className='summary-content'>
                                 <span>
                                     <ul className='accident-list'>
                                         <li>김민준 피자 사건</li>
                                         <li>룸메가 샤워하다가 타방출입 걸린 사건</li>
                                         <li>김석진 아침운동 없음 페이크 사건</li>
+                                        <li>방소연 선생님 하극상 사건</li>
+                                        <li>유근찬 선생님 노쇼 사건</li>
                                         <li>기말고사 주주원 급습 사건</li>
                                         <li>기숙사 내 애정행각 발각 사건</li>
                                         <li>기숙사 방문 9연속 파괴 사건</li>
                                         <li>샤워중 사감 난입 사건</li>
                                         <li>점호 50분 사건</li>
-                                    </ul>
-                                </span>
-                            </p>
-                        </AccodianMenu>
-                    </div>
-                    <div className='summary-wrap'>
-                        <AccodianMenu name={'선생님'}>
-                            <p className='summary-content'>
-                                <span>
-                                    <ul className='accident-list'>
-                                        <li>방소연 선생님 하극상 사건</li>
-                                        <li>유근찬 선생님 노쇼 사건</li>
                                     </ul>
                                 </span>
                             </p>
