b973f9225bf6584e5e134fe4ee8fd22dd571869a
Ubinquitous <ubinquitous1@gmail.com>
2022-11-11 08:53:48 +0900

    ADD : Link 추가

--- src/pages/Accident.tsx
+++ src/pages/Accident.tsx
@@ -9,6 +9,7 @@ import {
     Classify
 } from 'allFiles';
 import 'style/pages-style/Accident.scss'
+import { Link } from 'react-router-dom';
 
 const Accident = () => {
     return (
@@ -29,16 +30,16 @@ const Accident = () => {
                             <p className='summary-content'>
                                 <span>
                                     <ul className='accident-list'>
-                                        <li>피자만 가지고 가려고...</li>
-                                        <li>룸메가 샤워해서 벌점</li>
-                                        <li>김석진 : 힌남노에 의해 아침운동이 없을 예정입니다.</li>
-                                        <li>교무부장 선생님께 국어 선생님의 소심한 하극상</li>
-                                        <li>유근찬 선생님 노쇼</li>
-                                        <li>기말고사 당일 주주원 급습</li>
-                                        <li>기숙사 내 애정행각 발각</li>
-                                        <li>기숙사 방문 파괴자</li>
-                                        <li>샤워중인데 사감쌤 들어옴</li>
-                                        <li>점호 50분</li>
+                                        <li><Link to={`/accident/${1}`} className='link'>피자만 가지고 가려고...</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>룸메가 샤워해서 벌점</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>김석진 : 힌남노에 의해 아침운동이 없을 예정입니다.</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>교무부장 선생님께 국어 선생님의 소심한 하극상</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>유근찬 선생님 노쇼</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>기말고사 당일 주주원 급습</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>기숙사 내 애정행각 발각</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>기숙사 방문 파괴자</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>샤워중인데 사감쌤 들어옴</Link></li>
+                                        <li><Link to={`/accident/${1}`} className='link'>점호 50분</Link></li>
                                     </ul>
                                 </span>
                             </p>
