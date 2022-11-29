e3b13afb99ee68b363c218cb5ccff83d04563509
5ewon06 <gwonsewon03@gmail.com>
2022-11-29 11:23:49 +0900

    DELETE : 공백 수정

--- src/pages/Accident.tsx
+++ src/pages/Accident.tsx
@@ -6,7 +6,8 @@ import {
     Aside,
     Footer,
     AccodianMenu,
-    Classify
+    Classify,
+    ScrollBtn,
 } from 'allFiles';
 import 'style/pages-style/Accident.scss'
 import { Link } from 'react-router-dom';
@@ -47,6 +48,7 @@ const Accident = () => {
                     </div>
                     <SubFooter />
                 </Board>
+                <ScrollBtn />
                 <Aside />
             </div>
             <Footer />
