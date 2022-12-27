f6c6973ecdf045251fd6b3fd39ae2006c45c3046
Ubinquitous <ubinquitous1@gmail.com>
2022-12-27 22:35:24 +0900

    FIX : 테스트 케이스 변경

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -16,7 +16,7 @@ import { Link } from 'react-router-dom';
 const Student = () => {
     console.log(Dummy);
     const [stdId, setStdId] = useState('');
-    function ChangeId(e:string){
+    function ChangeId(e: string) {
         setStdId(e);
     }
     return (
@@ -49,15 +49,10 @@ const Student = () => {
                         <AccodianMenu name={'2022학년도 입학생'}>
                             <p className='summary-content'>
                                 <span>
-                                    <ul className='student-list'>
-                                        <li><Link to={`/student/${1}`} className='link'>강웅빈</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>구윤하</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>권세원</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>김나현</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>김민석</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>김민준</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>김호현</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>문서빈</Link></li>
+                                    <ul className="student-list">
+                                        {Dummy.student.map(student => (
+                                            <li><Link to={`/student/${student.name}`} className="link">{student.name}</Link></li>
+                                        ))}
                                     </ul>
                                 </span>
                             </p>
@@ -67,16 +62,10 @@ const Student = () => {
                         <AccodianMenu name={'2023학년도 입학생'}>
                             <p className='summary-content'>
                                 <span>
-                                    <ul className='student-list'>
-                                        <li><Link to={`/student/${stdId}`} className='link' onClick={e => setStdId(e.currentTarget.text)}>게이</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>강지원</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>이재윤</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>이상준</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>김호현</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>이윤찬</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>오창민</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>박사밀</Link></li>
-                                        <li><Link to={`/student/${1}`} className='link'>석사밀</Link></li>
+                                    <ul className="student-list">
+                                        {Dummy.student.map(student => (
+                                            <li><Link to={`/student/${student.name}`} className="link">{student.name}</Link></li>
+                                        ))}
                                     </ul>
                                 </span>
                             </p>
