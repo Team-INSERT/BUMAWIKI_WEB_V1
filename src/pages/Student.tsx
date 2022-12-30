acbf1cdf2f07d29a2fe0104c049385ea5701eb14
Ubinquitous <ubinquitous1@gmail.com>
2022-12-30 15:34:52 +0900

    UPDATE : import 형식 변경, axios 통신

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,72 +1,70 @@
 import React from 'react'
-import {
-    Header,
-    Board,
-    SubFooter,
-    Aside,
-    Footer,
-    Classify,
-    AccodianMenu,
-    ScrollBtn,
-    Dummy
-} from 'allFiles';
+import * as C from 'allFiles';
 import '../style/pages-style/Student.scss'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Student = () => {
+    axios.get('http://172.103.51.102/docs/find/1/version')
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     return (
         <div>
-            <Header />
+            <C.Header />
             <div className="student-board-wrap">
-                <Board>
+                <C.Board>
                     <div className="doc-title-box">
                         <span>부마위키:학생</span>
                     </div>
                     <div className="classif-box">
-                        <Classify>학생</Classify>
+                        <C.Classify>학생</C.Classify>
                     </div>
                     <div className="line" />
                     {/* 입학년도별 입학생 */}
                     <div className='summary-wrap'>
-                        <AccodianMenu name={'2021학년도 입학생'}>
+                        <C.AccodianMenu name={'2021학년도 입학생'}>
                             <ul className="student-list">
-                                {Dummy.student.map(student => (<>
+                                {C.Dummy.student.map(student => (<>
                                     {student.enroll === 2021 ?
                                         <li><Link to={`/student/${student.name}`} className='link'>{student.name}</Link></li> : ''}
                                 </>))}
                             </ul>
-                        </AccodianMenu>
+                        </C.AccodianMenu>
                     </div>
                     <div className='summary-wrap'>
-                        <AccodianMenu name={'2022학년도 입학생'}>
+                        <C.AccodianMenu name={'2022학년도 입학생'}>
                             <p className='summary-content'>
                                 <ul className="student-list">
-                                    {Dummy.student.map(student => (<>
+                                    {C.Dummy.student.map(student => (<>
                                         {student.enroll === 2022 ?
                                             <li><Link to={`/student/${student.name}`} className='link'>{student.name}</Link></li> : ''}
                                     </>))}
                                 </ul>
                             </p>
-                        </AccodianMenu>
+                        </C.AccodianMenu>
                     </div>
                     <div className='summary-wrap'>
-                        <AccodianMenu name={'2023학년도 입학생'}>
+                        <C.AccodianMenu name={'2023학년도 입학생'}>
                             <p className='summary-content'>
                                 <ul className="student-list">
-                                    {Dummy.student.map(student => (<>
+                                    {C.Dummy.student.map(student => (<>
                                         {student.enroll === 2023 ?
                                             <li><Link to={`/student/${student.name}`} className='link'>{student.name}</Link></li> : ''}
                                     </>))}
                                 </ul>
                             </p>
-                        </AccodianMenu>
+                        </C.AccodianMenu>
                     </div>
-                    <SubFooter />
-                </Board>
-                <ScrollBtn />
-                <Aside />
+                    <C.SubFooter />
+                </C.Board>
+                <C.ScrollBtn />
+                <C.Aside />
             </div>
-            <Footer />
+            <C.Footer />
         </div>
     );
 };
