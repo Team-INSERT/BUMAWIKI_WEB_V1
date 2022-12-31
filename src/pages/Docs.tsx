8a555091bc5551494c64e7c3311e57fdec67916d
Ubinquitous <ubinquitous1@gmail.com>
2022-12-31 21:01:06 +0900

    ADD : docs api 통신

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Docs = () => {
     const router = useParams();
+    const [docs, setDocs] = useState();
+
+    axios.get(`/docs/find/id/${router.id}`)
+        .then((res) => {
+            console.log(res)
+            setDocs(res.data)
+        })
+        .catch((err) => {
+            if (err instanceof AxiosError) {
+                console.log(err);
+                // alert('오류가 발생하여 문서를 불러올 수 없습니다.');
+            }
+        })
     return (
         <div>
             Docs id : {router.id}
