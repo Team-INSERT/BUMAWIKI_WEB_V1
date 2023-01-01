f0b0c2f595e7ca15edb6504e43a8baf2a0e536ce
Ubinquitous <ubinquitous1@gmail.com>
2023-01-01 23:13:51 +0900

    ADD, UPDATE : 최근 수정된 문서 파싱

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,30 +1,38 @@
+import { changeKor } from 'allFiles';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/components-style/Aside.scss';
+
+interface Docs {
+    title: string,
+    docsType: string,
+    id: number,
+}
 
 const Aside = () => {
     const [lastModifiedDocs, setLastModifiedDocs] = useState([]);
 
-    axios.get('/docs/find/modified')
-        .then((res) => {
-            console.log(res)
-            setLastModifiedDocs(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-            // alert('오류가 발생하여 문서를 불러올 수 없습니다.');
-        })
+    useEffect(() => {
+        axios.get('/docs/find/modified')
+            .then((res) => {
+                setLastModifiedDocs(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+                // alert('오류가 발생하여 문서를 불러올 수 없습니다.');
+            })
+    }, []);
 
     return (
         <div className='aside-wrap'>
             {/* 글자가 넘어갈 경우 슬라이싱 필요, map 사용 필요 (요소개수 : 10개) */}
             <div className='aside-title-box'>
-                <span>인기 문서</span>
+                <span>최근 수정된 문서</span>
             </div>
-            {lastModifiedDocs.map((docs) => (
-                <div className='aside-doc-box'>
-                    <Link to={'/docs/id'} className='list'>{'title'} ({'docsType'})</Link>
+            {lastModifiedDocs.map((docs: Docs) => (
+                <div className='aside-doc-box' key={docs.id}>
+                    <Link to={'/docs/id'} className='list'>{docs.title} ({changeKor(docs.docsType)})</Link>
                 </div>
             ))}
         </div>
