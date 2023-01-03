4ed9a12650a8478280c2c9f1a1aecbaaf3721e99
Ubinquitous <ubinquitous1@gmail.com>
2023-01-03 10:18:31 +0900

    FIX : 변경 사항 충돌 수정

---
+++ src/pages/Docs.tsx
@@ -0,0 +1,70 @@
+import * as C from 'allFiles';
+import axios, { AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { changeKor } from 'util/changeKor';
+import '../style/pages-style/Docs.scss'
+
+const Docs = () => {
+    const router = useParams();
+    const [docs, setDocs]: any = useState();
+    const [isLoad, setIsLoad] = useState(false);
+
+    useEffect(() => {
+        axios.get(`/docs/find/id/${router.id}`)
+            .then((res) => {
+                console.log(res)
+                setDocs(res.data)
+                setIsLoad(true);
+            })
+            .catch((err) => {
+                if (err instanceof AxiosError) {
+                    console.log(err);
+                    alert('오류가 발생하여 문서를 불러올 수 없습니다.');
+                }
+            })
+    }, [router.id]);
+    return (
+        <div>
+            <C.Header />
+            <div className="docs-board-wrap">
+                <C.Board>
+                    <div className="docs-title-box">
+                        <span>{docs?.title}</span>
+                    </div>
+                    <div className="classif-box">
+                        <C.Classify>{changeKor(docs?.docsType)}</C.Classify>
+                    </div>
+                    <div className="line" />
+                    <div className='summary-wrap'>
+                        {isLoad ?
+                            (
+                                <div>
+                                    <C.AccodianMenu name="개요">
+                                        이름 : {docs.title}<br />
+                                        입학년도 : {docs.enroll}년<br />
+                                        분류 : {changeKor(docs.docsType)}<br />
+                                        {docs.contents}<br />
+                                    </C.AccodianMenu>
+                                    <C.AccodianMenu name="어록">
+                                        마지막 수정 날짜 : {docs.lastModifiedAt.replace('T', ' ')}
+                                    </C.AccodianMenu>
+                                    <C.AccodianMenu name="마지막 수정일">
+                                        마지막 수정 날짜 : {docs.lastModifiedAt.replace('T', ' ')}
+                                    </C.AccodianMenu>
+                                </div>
+                            )
+                            : ''
+                        }
+                    </div>
+                    <C.SubFooter />
+                </C.Board>
+                <C.ScrollBtn />
+                <C.Aside />
+            </div>
+            <C.Footer />
+        </div>
+    );
+};
+
+export default Docs;
