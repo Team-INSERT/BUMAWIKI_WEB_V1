7fa827709f00de419df6cfa21113ac49e375ba75
Ubinquitous <ubinquitous1@gmail.com>
2023-01-06 23:36:31 +0900

    ADD : 어록 태그 추가

--- src/util/documentation.ts
+++ src/util/documentation.ts
@@ -1,11 +1,13 @@
 export const documentation = (content: string) => {
     const ORIGINAL_CONTENT = content
         .replace(/<항목>/gi, `?^li style="list-style: disc;"^?`)
+        .replace(/<어록>/gi, `?^div style="width:100%; background-color:*ccc; height:50px; border: 1px solid black; font-weight:800; display:flex; align-items:center;"^??^span style="margin-left:20px;"^?`)
+        .replace(/<\/어록>/gi, `?^#span^??^#div^?`)
         .replace(/<강조>/gi, `?^strong^?`)
         .replace(/<\/강조>/gi, `?^#strong^?`)
         .replace(/<취소선>/gi, `?^del style="color:#9D9D9D;"^?`)
         .replace(/<\/취소선>/gi, `?^#del^?`)
-        .replace(/<링크 문서={/gi, `?^a href='/search/`)
+        .replace(/<링크 문서={/gi, `?^a style="text-decoration:none; color:blue;" href='/search/`)
         .replace(/}>/gi, `'^?`)
         .replace(/<\/링크>/gi, `?^#a^?`)
         .replace(/<br\/>/gi, `?^br#^?`)
@@ -17,5 +19,6 @@ export const documentation = (content: string) => {
         .replace(/\?\^/gi, `<`)
         .replace(/\^\?/gi, `>`)
         .replace(/#/gi, `/`)
+        .replace(/\*/gi, `#`)
     return HTML_CONTENT;
 }
