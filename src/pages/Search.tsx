cf280be42f148ea1d20c73b45936d362fa18bbaf
Ubinquitous <ubinquitous1@gmail.com>
2023-01-06 15:13:01 +0900

    FIX : 검색결과가 하나일 경우 바로 해당 문서로 넘어가게 수정

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,17 +1,21 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Docs from 'types/docs';
 
 const Search = () => {
     const router = useParams();
+    const navigate = useNavigate();
     const [result, setResult] = useState([]);
+    const [isLoad, setIsLoad] = useState(false);
 
     useEffect(() => {
         axios.get(`/docs/find/title/${router.result}`)
             .then((res) => {
                 console.log(res)
                 setResult(res.data)
+                if (res.data.length === 1) navigate(`/docs/${res.data[0].id}`)
+                setIsLoad(true);
             })
             .catch((err) => {
                 console.log(err)
@@ -20,10 +24,11 @@ const Search = () => {
 
     return (
         <div>
-            <h1>'{router.result}' 검색결과</h1><br />
-            {result.map((result: Docs) => (
-                <Link to={`/docs/${result.id}`}>{result.title}</Link>
-            ))}
+            {isLoad ? <>
+                <h1>'{router.result}' 검색결과</h1><br />
+                {result.map((result: Docs, index) => (
+                    <Link key={index} to={`/docs/${result.id}`}>{result.title}</Link>
+                ))}</> : ''}
         </div>
     );
 };
