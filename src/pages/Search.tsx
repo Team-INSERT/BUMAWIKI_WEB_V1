f59478f874bb19754ac6e35bd4fd802a5ab79d2c
5ewon06 <gwonsewon03@gmail.com>
2023-01-08 01:38:42 +0900

    ADD : search layout

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import * as C from 'allFiles';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import Docs from 'types/docs';
+import '../style/pages-style/Search.scss'
 
 const Search = () => {
     const router = useParams();
@@ -25,11 +27,34 @@ const Search = () => {
 
     return (
         <div>
-            {isLoad ? <>
-                <h1>'{router.result}' 검색결과</h1><br />
-                {result.map((result: Docs, index) => (
-                    <Link key={index} to={`/docs/${result.id}`}>{result.title}</Link>
-                ))}</> : ''}
+            <C.Header />
+            <div className="search-board-wrap">
+                <C.Board>
+                    <div className="search-title-box">
+                        <span>'{router.result}' 검색결과</span>
+                    </div>
+                    <div className="classif-box">
+                        <C.Classify>검색</C.Classify>
+                    </div>
+                    <div className="line" />
+                    <div className='summary-wrap'>
+                        <ul className="search-list">
+                            {isLoad ? <>
+                                {result.map((result: Docs, index) => (
+                                    <li><Link key={index} className="link" to={`/docs/${result.id}`}>{result.title}({result.enroll})</Link><br /><div className="search-line" /></li>
+                                ))}</> : <div>
+                                        <span>검색 결과가 존재하지 않습니다.</span><br />
+                                        <span>생성ㄱ?</span>
+                                    </div>
+                                }
+                        </ul>
+                    </div>
+                    <C.SubFooter />
+                </C.Board>
+                <C.ScrollBtn />
+                <C.Aside />
+            </div>
+            <C.Footer />
         </div>
     );
 };
