3c75c5ac1d632acad31413c216272ee0ad8d7c7f
Ubinquitous <ubinquitous1@gmail.com>
2023-01-07 15:06:12 +0900

    UPDATE : 오전 0시일 경우 오전 12시로 리턴하게 업데이트

--- src/util/dateParser.ts
+++ src/util/dateParser.ts
@@ -3,8 +3,8 @@ export const dateParser = (date: string) => {
     ${date.substring(0, 4)}년 
     ${parseInt(date.substring(5, 7)) - 10 >= 0 ? date.substring(5, 7) : date.substring(6, 7)}월 
     ${parseInt(date.substring(8, 10)) - 10 >= 0 ? date.substring(8, 10) : date.substring(9, 10)}일
-    ${parseInt(date.substring(11, 13)) >= 13 ? `오후 ${parseInt(date.substring(11, 13)) - 12}` : `오전 ${parseInt(date.substring(11, 13))}`}시 
-    ${date.substring(14, 16)}분`
+    ${parseInt(date.substring(11, 13)) >= 13 ? `오후 ${parseInt(date.substring(11, 13)) - 12}` : parseInt(date.substring(11, 13)) === 0 ? '오전 12' : `오전 ${parseInt(date.substring(11, 13))}`}시 
+    ${parseInt(date.substring(14, 16)) - 10 >= 0 ? date.substring(14, 16) : date.substring(15, 16)}분`
 
     return PARSING_DATE;
 }
