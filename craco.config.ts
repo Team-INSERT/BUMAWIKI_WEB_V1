60a292b4020e232a8c125413c7a56edd4e44153b
Ubinquitous <ubinquitous1@gmail.com>
2023-01-17 04:05:21 +0900

    DELETE : craco proxy 주석처리

--- craco.config.ts
+++ craco.config.ts
@@ -2,11 +2,11 @@ module.exports = {
     devServer: {
         historyApiFallback: true, // react router
         port: 4000,
-        proxy: {
-            '/api/': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-            },
-        },
+        // proxy: {
+        //     '/api/': {
+        //         target: 'http://localhost:8080',
+        //         changeOrigin: true,
+        //     },
+        // },
     },
 };
