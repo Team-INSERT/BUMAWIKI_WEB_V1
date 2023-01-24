30eb8f75a0f675b6b195af8d61449a4d88673a85
Ubinquitous <ubinquitous1@gmail.com>
2023-01-24 15:46:16 +0900

    ADD : react-query setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 0,
+			suspense: false,
+		},
+	},
+	queryCache: new QueryCache({
+		onError: (err) => {
+			console.log(err)
+		},
+	}),
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <App />
+	<QueryClientProvider client={queryClient}>
+		<ReactQueryDevtools initialIsOpen={true} />
+		<App />
+	</QueryClientProvider>
 )
