434846827a994b7cf2bcf7bed6860eab81155670
Ubinquitous <ubinquitous1@gmail.com>
2023-01-26 12:21:13 +0900

    ADD : version docs type

--- src/types/docs.ts
+++ src/types/docs.ts
@@ -1,11 +1,11 @@
 interface Docs {
-    id: number,
-    title: string,
-    enroll?: number,
-    contents: string,
-    docsType: string,
-    view: number,
-    lastModifiedAt: ''
+	id: number
+	title: string
+	enroll?: number
+	contents: string
+	docsType: string
+	view: number
+	lastModifiedAt: ''
 }
 
 export default Docs
---
+++ src/types/versionDocs.ts
@@ -0,0 +1,7 @@
+interface VersionDocs {
+	thisVersionCreatedAt: string
+	userId: number
+	nickName: string
+}
+
+export default VersionDocs
