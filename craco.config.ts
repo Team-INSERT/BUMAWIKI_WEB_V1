b3d3d52d498ed80bd29cfc180a86428d04be9ff2
Ubinquitous <ubinquitous1@gmail.com>
2023-02-06 23:49:35 +0900

    FIX

--- craco.config.ts
+++ craco.config.ts
@@ -1,12 +1,12 @@
 module.exports = {
-    devServer: {
-        historyApiFallback: true, // react router
-        port: 4000,
-        // proxy: {
-        //     '/api/': {
-        //         target: 'http://localhost:8080',
-        //         changeOrigin: true,
-        //     },
-        // },
-    },
-};
+	devServer: {
+		historyApiFallback: true, // react router
+		port: 4000,
+		// proxy: {
+		//     '/api/': {
+		//         target: 'http://localhost:8080',
+		//         changeOrigin: true,
+		//     },
+		// },
+	},
+}
