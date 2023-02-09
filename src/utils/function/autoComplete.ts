623ea68b25aa60bd01f7863c8ceccdcc7fe6b72b
Ubinquitous <ubinquitous1@gmail.com>
2023-02-09 13:52:27 +0900

    UPDATE : close tag 로직 변경

--- src/utils/function/autoComplete.ts
+++ src/utils/function/autoComplete.ts
@@ -1,40 +1,36 @@
 const autoComplete = (contents: string, e: React.ChangeEvent<HTMLTextAreaElement>) => {
-	if (
-		contents.substring(contents.length - 3, contents.length) === '<강조' ||
-		contents.substring(contents.length - 3, contents.length) === '<어록' ||
-		contents.substring(contents.length - 3, contents.length) === '<빨강' ||
-		contents.substring(contents.length - 3, contents.length) === '<하양' ||
-		contents.substring(contents.length - 3, contents.length) === '<노랑'
-	) {
+	console.log(e)
+	if (e.target.value[e.target.selectionStart - 1] === '>') {
+		const selection = e.target.selectionStart
+		let text = e.target.value.slice(0, e.target.selectionStart)
+		const tag = text.substring(text.lastIndexOf('<') + 1, text.length)
+
+		if (tag.includes('/')) return e.target.value
+		text += `</${tag}` + e.target.value.slice(e.target.selectionStart, e.target.value.length)
 		setTimeout(() => {
-			e.target.selectionStart = contents.length + 1
-			e.target.selectionEnd = contents.length + 1
-		}, 10)
-		return `${contents}></${contents.substring(contents.length - 2, contents.length)}>`
-	} else if (
-		contents.substring(contents.length - 4, contents.length) === '<취소선' ||
-		contents.substring(contents.length - 4, contents.length) === '<소제목'
-	) {
-		setTimeout(() => {
-			e.target.selectionStart = contents.length + 1
-			e.target.selectionEnd = contents.length + 1
-		}, 10)
-		return `${contents}></${contents.substring(contents.length - 3, contents.length)}>`
-	} else if (contents.substring(contents.length - 3, contents.length) === '<링크') {
-		setTimeout(() => {
-			e.target.selectionStart = contents.length + 5
-			e.target.selectionEnd = contents.length + 5
-		}, 10)
-		return `${contents} 문서={}></링크>`
-	} else if (contents.substring(contents.length - 5, contents.length) === '<외부링크') {
-		setTimeout(() => {
-			e.target.selectionStart = contents.length + 5
-			e.target.selectionEnd = contents.length + 5
-		}, 10)
-		return `${contents} 문서={}></외부링크>`
-	} else {
-		return e.target.value
+			e.target.selectionStart = selection
+			e.target.selectionEnd = selection
+		}, 0)
+		return text
 	}
+	return e.target.value
 }
 
 export default autoComplete
+
+// const autoComplete = (contents: string, e: React.ChangeEvent<HTMLTextAreaElement>) => {
+// 	let text = ''
+// 	if (e.target.value.lastIndexOf('>') !== -1 && !e.target.value[e.target.value.lastIndexOf('>') + 1]) {
+// 		console.log(e.target.value[e.target.value.lastIndexOf('>') + 1])
+// 		const TAG = e.target.value.substring(e.target.value.lastIndexOf('<') + 1, e.target.value.lastIndexOf('>') + 1)
+// 		e.target.value += `</${TAG}`
+
+// 		e.target.selectionStart = e.target.value.lastIndexOf('>') + 1
+// 		e.target.selectionEnd = e.target.value.lastIndexOf('>') + 1
+
+// 		text += e.target.value
+// 	}
+// 	return e.target.value
+// }
+
+// export default autoComplete
