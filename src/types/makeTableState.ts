d3415fe984822b59906af2f52285ebc32ba0eee1
Ubinquitous <ubinquitous1@gmail.com>
2023-02-20 16:55:06 +0900

    ADD : state값 타입 지정

---
+++ src/types/makeTableState.ts
@@ -0,0 +1,14 @@
+interface MakeTableState {
+	Color: string
+	TextColor: string
+	Line: string
+	Name: string
+	Height: string
+	Birth: string
+	Country: string
+	MBTI: string
+	Club: string
+	Field: string
+}
+
+export default MakeTableState
