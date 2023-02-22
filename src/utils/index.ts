aa6bbddf4c43b34ed055e18015cb33f06d39eb7b
Ubinquitous <ubinquitous1@gmail.com>
2023-02-22 12:30:12 +0900

    DELETE : 테이블 생성기

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,21 +5,8 @@ import dateUTCParser from './date/dateUTCParser'
 import documentation from './document/documentation'
 import getCookie from './etc/getCookie'
 import getLastDate from './date/getLastDate'
-import makeTable from './document/makeTable'
 import typeEditor from './etc/typeEditor'
 import onKeyDownUseTab from './document/onKeyDownUseTab'
 import getAllYear from './date/getAllYear'
 
-export {
-	asideFormat,
-	autoClosingTag,
-	dateParser,
-	dateUTCParser,
-	documentation,
-	getCookie,
-	getLastDate,
-	makeTable,
-	typeEditor,
-	onKeyDownUseTab,
-	getAllYear,
-}
+export { asideFormat, autoClosingTag, dateParser, dateUTCParser, documentation, getCookie, getLastDate, typeEditor, onKeyDownUseTab, getAllYear }
