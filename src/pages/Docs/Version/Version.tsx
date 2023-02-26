1b8936513d2be6be8792a440a9e4b4735c70a051
Ubinquitous <ubinquitous1@gmail.com>
2023-02-26 15:30:16 +0900

    ADD : 문서 수정 기록에 이름 뜨게 수정

--- src/pages/Docs/Version/Version.tsx
+++ src/pages/Docs/Version/Version.tsx
@@ -13,10 +13,12 @@ const Version = () => {
 	const router = useParams()
 	const [version, setVersion] = React.useState([])
 	const [isLoad, setIsLoad] = React.useState(false)
+	const [docsName, setDocsName] = React.useState()
 
 	useQuery('versionDocs', () => api.getVersionDocs(router.title as string), {
 		onSuccess: (data) => {
 			setVersion(data.versionDocsResponseDto.reverse())
+			setDocsName(data.docsResponseDto.title)
 			setIsLoad(true)
 		},
 		onError: (err) => {
@@ -31,7 +33,7 @@ const Version = () => {
 			<S.DocsWrap>
 				<C.Board>
 					<S.DocsTitleWrap>
-						<S.DocsTitleText>문서 수정 기록</S.DocsTitleText>
+						<S.DocsTitleText>문서 수정 기록 : {docsName}</S.DocsTitleText>
 					</S.DocsTitleWrap>
 					<S.DocsLine />
 					<S.DocsContentsWrap>
