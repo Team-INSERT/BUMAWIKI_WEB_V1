18c701bfb29ebc211e3790c1db8a38e0771d5567
Ubinquitous <ubinquitous1@gmail.com>
2023-03-06 00:14:06 +0900

    ADD : 한국어 패치

--- src/utils/date/getLastDate.ts
+++ src/utils/date/getLastDate.ts
@@ -1,19 +1,11 @@
 import moment from 'moment'
+import 'moment/locale/ko'
 
 const getLastDate = (UTC: string) => {
 	const date = new Date(UTC)
 	const lastModifiedAt = [date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]
 
-	const toNow = moment(lastModifiedAt).toNow().replace('in ', '').replace('s', '').replace(' ago', '')
-
-	if (toNow.includes('second')) return toNow.replace('a few seconds', '몇 초전')
-	if (toNow.includes('minute')) return toNow.replace(' minute', '분 전').replace('a', '1')
-	if (toNow.includes('hour')) return toNow.replace(' hour', '시간 전').replace('an', '1')
-	if (toNow.includes('day')) return toNow.replace(' day', '일 전').replace('a', '1')
-	if (toNow.includes('month')) return toNow.replace(' month', '달 전').replace('a', '1')
-	if (toNow.includes('year')) return toNow.replace(' year', '일 전').replace('a', '1')
-
-	return ''
+	return moment(lastModifiedAt).fromNow()
 }
 
 export default getLastDate
