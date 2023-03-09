d18367561245a23a624a647176eb9c87c44a3db2
Ubinquitous <ubinquitous1@gmail.com>
2023-03-09 17:32:17 +0900

    ADD : 틀 type

--- src/utils/etc/typeEditor.ts
+++ src/utils/etc/typeEditor.ts
@@ -1,27 +1,16 @@
 const typeEditor = (classify: string) => {
-	if (classify === 'STUDENT') {
-		return '학생'
-	} else if (classify === 'ACCIDENT') {
-		return '사건/사고'
-	} else if (classify === 'CLUB') {
-		return '전공 동아리'
-	} else if (classify === 'TEACHER') {
-		return '인문교과 선생님'
-	} else if (classify === 'MAJOR_TEACHER') {
-		return '전문교과 선생님'
-	} else if (classify === 'MENTOR_TEACHER') {
-		return '멘토 선생님'
-	} else if (classify === 'FREE_CLUB') {
-		return '사설 동아리'
-	} else if (classify === 'ADMIN') {
-		return '관리자'
-	} else if (classify === 'BANNED') {
-		return '읽기전용 사용자'
-	} else if (classify === 'USER') {
-		return '사용자'
-	} else if (classify === 'NOTICE') {
-		return '공지사항'
-	}
+	if (classify === 'STUDENT') return '학생'
+	if (classify === 'ACCIDENT') return '사건/사고'
+	if (classify === 'CLUB') return '전공 동아리'
+	if (classify === 'TEACHER') return '인문교과 선생님'
+	if (classify === 'MAJOR_TEACHER') return '전문교과 선생님'
+	if (classify === 'MENTOR_TEACHER') return '멘토 선생님'
+	if (classify === 'FREE_CLUB') return '사설 동아리'
+	if (classify === 'ADMIN') return '관리자'
+	if (classify === 'BANNED') return '읽기전용 사용자'
+	if (classify === 'USER') return '사용자'
+	if (classify === 'NOTICE') return '공지사항'
+	if (classify === 'FRAME') return '틀'
 	return classify
 }
 
