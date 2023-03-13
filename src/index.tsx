60c9588a2b1471fb9763fabe054ebd41e2fa8e79
Ubinquitous <ubinquitous1@gmail.com>
2023-03-13 10:56:44 +0900

    FIX : 예외처리

--- src/index.tsx
+++ src/index.tsx
@@ -18,14 +18,11 @@ const queryClient = new QueryClient({
 	queryCache: new QueryCache({
 		onError: (err) => {
 			if (err instanceof AxiosError) {
-				const { status, code, message } = err
-				if (message && code) {
-					if (message === 'Cannot Change Your Docs') alert('자기자신의 문서는 변경할 수 없습니다.')
-					else if (message === 'YOUR BANNED') alert('읽기전용 유저는 문서를 편집할 수 없습니다.')
-					else if (status === 403) alert('로그인 후 이용 가능한 서비스입니다.')
-					else if (status === 404) alert('잘못된 접근입니다.')
-					else if (status === 500) alert('서버에 오류가 발생했습니다.')
-				}
+				const { status } = err
+				if (status === 403) return alert('로그인 후 이용 가능한 서비스입니다.')
+				if (status === 404) return alert('잘못된 접근입니다.')
+				if (status === 500) return alert('서버에 오류가 발생했습니다.')
+				return alert('오류가 발생하여 문서를 불러올 수 없습니다.')
 			}
 		},
 	}),
