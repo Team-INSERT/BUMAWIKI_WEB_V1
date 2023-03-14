465423792a658c2c2265cded04df5d8f7b47348c
Ubinquitous <ubinquitous1@gmail.com>
2023-03-15 01:56:39 +0900

    FIX : undefined

--- src/lib/token/tokenExpired.ts
+++ src/lib/token/tokenExpired.ts
@@ -4,9 +4,13 @@ import delCookie from 'utils/etc/delCookie'
 
 const tokenExpired = async () => {
 	try {
-		const res = await bumawikiAxios.put('/auth/refresh/access', {
-			refresh_token: getCookie('refresh_token'),
-		})
+		const res = await bumawikiAxios.put(
+			'/auth/refresh/access',
+			{
+				refresh_token: getCookie('refresh_token'),
+			},
+			{}
+		)
 		document.cookie = `authorization=${res.data.accessToken};`
 	} catch (err) {
 		delCookie('authorization')
