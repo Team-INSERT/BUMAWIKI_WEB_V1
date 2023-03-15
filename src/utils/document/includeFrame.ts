89d1478a1116c56abb3d080cc0e71a008c98b358
Ubinquitous <ubinquitous1@gmail.com>
2023-03-15 19:05:12 +0900

    FIX

--- src/utils/document/includeFrame.ts
+++ src/utils/document/includeFrame.ts
@@ -1,9 +1,13 @@
 import * as api from 'api/getDocs'
 
 const includeFrame = async (frameTitle: string) => {
-	const res = await api.getDocs(frameTitle)
-	if (res.docsType === 'FRAME') return res.contents
-	return '포함할 대상이 틀이 아닙니다'
+	try {
+		const res = await api.getDocs(frameTitle)
+		if (res.docsType === 'FRAME') return res.contents
+		return '포함할 대상이 틀이 아닙니다'
+	} catch (err) {
+		return '존재하지 않는 틀입니다.'
+	}
 }
 
 export default includeFrame
